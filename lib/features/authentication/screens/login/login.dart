f6cf034e0a8a44f5c11e1faec75eda8dc94c6f8c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-11-21 14:44:11 +0530

    Login Page UI  done

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/styles/spacing_styles.dart';
+import 'package:t_store/common/widgets/login_signup/form_divider.dart';
+import 'package:t_store/common/widgets/login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_form.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_header.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: SpacingStyles.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              //logo, title and subtitle
+              TLoginHeader(dark: dark),
+
+              //Form
+              const TLoginForm(),
+
+              //Divider
+              TFormDevider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              //Footer
+              const TSocialButtons()
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
