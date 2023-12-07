48cd8c9826f04026c07aa328154212b5bb0836ce
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-12-07 14:09:27 +0530

    Product Detail Page UI done

---
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:t_store/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:t_store/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return   const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Product Image slider
+            ProductImageSlider(),
+
+            //Product Details
+            Padding(padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
+            child: Column(
+              children: [
+                //Rating & share button
+                RatingAndShare(),
+
+                //Price , title, stock and brand
+                ProductMetaData()
+
+                //Attributes
+
+                //Checkout Button
+
+                //Description
+
+                //Reviews
+              ],
+            ),
+             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/features/shop/screens/product_details/widgets/product_detail_image_slider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:t_store/common/widgets/icons/circular_icon.dart';
+import 'package:t_store/common/widgets/images/rounded_image.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class ProductImageSlider extends StatelessWidget {
+  const ProductImageSlider({
+    super.key,
+    
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: dark? TColors.darkGrey : TColors.light,
+        child: Stack(
+          children: [
+            //Main Large image
+            const SizedBox(height: 400,child: Padding(
+              padding: EdgeInsets.all(TSizes.productImageRadius *2),
+              child: Center(child: Image(image: AssetImage(TImages.productImage5))),
+            )),
+    
+            //Image slider
+            Positioned(
+              right: 0,
+              bottom: 30,
+              left: TSizes.defaultSpace,
+              child: SizedBox(
+                height: 80,
+                child: ListView.separated(
+                  separatorBuilder: (_,__)=>const SizedBox(width: TSizes.spaceBtwItems)
+                  ,itemCount: 6,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (_,index)=> RoundedImage(
+                    width: 80,
+                    backgroundColor: dark? TColors.dark: TColors.white,
+                    border: Border.all(color: TColors.primary),
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    imageUrl: TImages.productImage3)),
+              ),
+            ),
+    
+            //Appbar icons
+            const TAppBar(
+              showBackArrow: true,
+              actions: [
+                CircularIcon(icon: Iconsax.heart5, color: Colors.red,)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
