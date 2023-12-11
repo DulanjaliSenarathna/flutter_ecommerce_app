545a4f4801842ff8278a8038d39ef3f4b6280612
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-12-11 08:53:35 +0530

    Onboarding Screen Only One Time done

--- lib/features/shop/screens/brand/all_brands.dart
+++ lib/features/shop/screens/brand/all_brands.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/brands/brand_card.dart';
 import 'package:t_store/common/widgets/layout/grid_layout.dart';
-import 'package:t_store/common/widgets/products/sortable/sortable_products.dart';
 import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/features/shop/screens/brand/brand_products.dart';
 import 'package:t_store/utils/constants/sizes.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:t_store/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:t_store/data/repositories/authentication/authentication_repository.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  //Add widget binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  //Get local storage
+  await GetStorage.init();
+
+  //Await splash screen until other items load
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   //Initialize firebase
-await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
   runApp(const App());
 }
